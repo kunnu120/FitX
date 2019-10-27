4d2492ac846a558b70e09ddc82c38e9198afd3be
William Simmons <simzwill98@gmail.com>
2019-10-27 19:37:54 -0400

    #Work on OurAuthActivity -Build on OurAuthActivity

--- app/src/main/java/com/google/firebase/androidx/fitx/java/OurAuthActivity.java
+++ app/src/main/java/com/google/firebase/androidx/fitx/java/OurAuthActivity.java
@@ -1,4 +1,27 @@
 package com.google.firebase.androidx.fitx.java;
 
-public class OurAuthActivity {
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.quickstart.auth.R;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+
+
+public class OurAuthActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "OurAuthActivity";
+
+    private FirebaseAuth oAuth;
+
 }
