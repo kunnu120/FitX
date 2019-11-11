ddb50b60c4f6898164d37e57bfd67db269417f5a
William Simmons <simzwill98@gmail.com>
2019-11-11 01:34:28 -0500

    # code changes - Created navigation bar for three page access (home,share, profile)

--- app/src/main/java/com/example/fitx/HomeActivity.java
+++ app/src/main/java/com/example/fitx/HomeActivity.java
@@ -2,12 +2,18 @@ package com.example.fitx;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -17,14 +23,34 @@ public class HomeActivity extends AppCompatActivity {
     FirebaseAuth fAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
 
+    //Menu BottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         textV = findViewById(R.id.textView);
         logoutButton = findViewById(R.id.logoutButton);
-
         fAuth = FirebaseAuth.getInstance();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.navigation_share:
+                        Toast.makeText(HomeActivity.this, "Share", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.navigation_profile:
+                        Toast.makeText(HomeActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
 
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
