3a1eb9260ffb7b54b5e7ad47829242c9d81ebaf4
SeanWiig <seanwiig@gmail.com>
2020-02-02 01:23:07 -0500

    Add test or new functionality

---
+++ app/src/test/java/com/example/fitx/HomeTest.java
@@ -0,0 +1,12 @@
+package com.example.fitx;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(HomeFragment.dummyFunction(), "This is a useful test.");
+    }
+}
