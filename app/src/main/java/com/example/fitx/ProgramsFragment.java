91ae19982fcffd3315f72924dff1b1987631288b
William Simmons <simzwill98@gmail.com>
2020-02-27 18:16:25 -0500

    Merge branch 'master' of https://github.com/SCCapstone/FitX into WilliamsBranch
    Added Programs page Calendar along with
    setup of program comment section
    
    # Conflicts:
    #	app/src/main/java/com/example/fitx/HomeActivity.java
    #	app/src/main/res/layout/activity_home.xml
    #	app/src/main/res/layout/fragment_home.xml
    #	app/src/main/res/menu/bottom_nav_menu.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/fitx/ProgramsFragment.java
+++ app/src/main/java/com/example/fitx/ProgramsFragment.java
@@ -1,15 +1,29 @@
 package com.example.fitx;
 
+import android.graphics.Color;
+import android.text.format.DateFormat;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.time.Year;
+import java.util.Calendar;
 
-public class ProgramsFragment extends Fragment {
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
+
+
+
+public class ProgramsFragment extends Fragment{
+
+    private HorizontalCalendar horizontalCalendar;
+
     @NonNull
     @Override
     public View onCreateView(LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
@@ -17,6 +31,39 @@ public class ProgramsFragment extends Fragment {
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_programs, null);
+
+        View rootView = inflater.inflate(R.layout.fragment_programs, container, false);
+        /* starts before 1 month from now */
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.MONTH, -1);
+
+        /* ends after 1 month from now */
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH, 1);
+
+        horizontalCalendar = new HorizontalCalendar.Builder(rootView, R.id.calendarView)
+                .range(startDate, endDate)
+                .datesNumberOnScreen(5)
+                .configure()
+                    .formatTopText("MMM")
+                    .formatMiddleText("dd")
+                    .formatBottomText("EEE")
+                    .textSize(14f, 24f, 14f)
+                    .showTopText(true)
+                    .showBottomText(true)
+                    .textColor(Color.LTGRAY, Color.WHITE)
+                .end()
+                .build();
+
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            public void onDateSelected(Calendar date, int position) {
+                Toast.makeText(getContext(), DateFormat.format("EEE, MMM d, yyyy", date) + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        return rootView;
+        //return inflater.inflate(R.layout.fragment_programs, null);
     }
 }
