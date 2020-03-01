cc3cb2ab5f99fffaaa3e4106f873952aa15d73ab
William Simmons <simzwill98@gmail.com>
2020-03-01 15:33:37 -0500

    Merge branch 'master' of https://github.com/SCCapstone/FitX into WilliamsBranch
    Added Programs page Calendar along with
    setup of program comment section
    Added the date selected
    Created date selected output box
    
    # Conflicts:
    #	app/src/main/java/com/example/fitx/HomeActivity.java
    #	app/src/main/res/layout/activity_home.xml
    #	app/src/main/res/layout/fragment_home.xml
    #	app/src/main/res/menu/bottom_nav_menu.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/fitx/ProgramsFragment.java
+++ app/src/main/java/com/example/fitx/ProgramsFragment.java
@@ -5,18 +5,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.text.format.DateFormat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 //import java.text.DateFormat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 
 import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.model.CalendarEvent;
+import devs.mulham.horizontalcalendar.utils.CalendarEventsPredicate;
 import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
 
 
@@ -25,20 +41,74 @@ public class ProgramsFragment extends Fragment{
 
     private HorizontalCalendar horizontalCalendar;
     private TextView dateSelected;
-    private ListView datePrint;
+    private ScrollView dateScroll;
+    private ListView dateListPrint;
+    private ListView programList;
+
+    //array lists for programs and exercises
+    private ArrayList<String> programs;
+    private ArrayAdapter<String> programsAdapter;
+
+    //array lists for dates
+    private ArrayList<Calendar> datesClicked;
+    private ArrayAdapter<Calendar> datesClickedAdapter;
+
+    //initialize and declare database reference
+    private final FirebaseDatabase db = FirebaseDatabase.getInstance();
+    //initialized reference for Programs
+    private DatabaseReference userPrograms;
+
+
+
+    private ValueEventListener programListener = new ValueEventListener(){
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            try {
+
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    programsAdapter.addAll((String) ds.getKey());
+                }
+
+
+            } catch (NullPointerException e) {
+
+            }
+        }
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError){
+
+        }
+    };
+
+    private ValueEventListener dateListener = new ValueEventListener(){
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            try {
+
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    datesClickedAdapter.add(Calendar.getInstance());
+                }
+
+
+            } catch (NullPointerException e) {
+
+            }
+        }
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError){
+
+        }
+    };
 
     @NonNull
     @Override
     public View onCreateView(LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
-        //just change the fragment_dashboard
-        //with the fragment you want to inflate
-        //like if the class is HomeFragment it should have R.layout.home_fragment
-        //if it is DashboardFragment it should have R.layout.fragment_dashboard
 
         View rootView = inflater.inflate(R.layout.fragment_programs, container, false);
         dateSelected = rootView.findViewById(R.id.dateBox);
-        datePrint = rootView.findViewById(R.id.dateOutput);
+        //dateScroll = rootView.findViewById(R.id.dateScroll);
 
+        ////////Calendar View Specifications////////
         /* starts before 1 month from now */
         Calendar startDate = Calendar.getInstance();
         startDate.add(Calendar.MONTH, -1);
@@ -58,19 +128,55 @@ public class ProgramsFragment extends Fragment{
                     .showBottomText(true)
                     .textColor(Color.LTGRAY, Color.WHITE)
                 .end()
+                .addEvents(new CalendarEventsPredicate() {
+
+                    Random rnd = new Random();
+                    @Override
+                    public List<CalendarEvent> events(Calendar date) {
+                        List<CalendarEvent> events = new ArrayList<>();
+                        int count = rnd.nextInt(6);
+
+                        for (int i = 0; i <= count; i++){
+                            events.add(new CalendarEvent(Color.rgb(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)), "event"));
+                        }
+
+                        return events;
+                    }
+                })
                 .build();
 
         horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
             @Override
             public void onDateSelected(Calendar date, int position) {
-                //dateSelected.setText("Date Selected: " + date);
-                //Toast.makeText(getContext(), DateFormat.format("EEE, MMM d, yyyy", date) + " is selected!", Toast.LENGTH_SHORT).show();
                 dateSelected.setText(DateFormat.format("EEE, MMM d, yyyy", date));
-            }
+                //dateScroll.addView((View) DateFormat.format("EEE, MMM d, yyyy", date));
+                //initializing add program to selected date button
+                //Button addProgram = rootView.findViewById(R.id.addProgramToDate);
 
+                //initializing dates selected list view
+                //dateListPrint = rootView.findViewById(R.id.dateOutputList);
+
+                //declare programs reference for user
+                //datesClicked = new ArrayList<Calendar>();
+                //datesClicked.add(date);
+                //userDates = db.getReference("Users").child(userid).child("Program Dates");
+                //userDates.addListenerForSingleValueEvent();
+                //DateListAdapter dla = new DateListAdapter(this, android.R.layout.simple_list_item_1, datesClicked);
+                //datesClickedAdapter = new ArrayAdapter<Calendar>(this,datesClicked );
+                //dateListPrint.setAdapter(dla);
+                //dateListPrint.setOnItemClickListener((AdapterView.OnItemClickListener) DateFormat.format("EEE, MMM d, yyyy", date));
+            }
+            @Override
+            public boolean onDateLongClicked(Calendar date, int position) {
+                //get current user id
+                //String userid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                //declare programs reference
+                //userPrograms = db.getReference("Users").child(userid).child("Programs");
+                //userPrograms.addListenerForSingleValueEvent(programListener);
+                return true;
+            }
         });
 
         return rootView;
-        //return inflater.inflate(R.layout.fragment_programs, null);
     }
 }
