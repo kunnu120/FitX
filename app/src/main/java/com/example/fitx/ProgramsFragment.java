a33821fb68baf91c977bbdb14c4d2e57e644c44e
William Simmons <simzwill98@gmail.com>
2020-03-09 11:22:45 -0400

    Merge branch 'master' of https://github.com/SCCapstone/FitX into WilliamsBranch
    Added Programs page Calendar along with
    setup of program comment section
    Added the date selected
    Created date selected output box
    
    # Conflicts:
    #	app/src/main/java/com/example/fitx/HomeActivity.java
    #	app/src/main/res/layout/activity_home.xml
    #	app/src/main/res/layout/fragment_home.xml
    #	app/src/main/res/menu/bottom_nav_menu.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/fitx/ProgramsFragment.java
+++ app/src/main/java/com/example/fitx/ProgramsFragment.java
@@ -1,19 +1,26 @@
 package com.example.fitx;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.text.format.DateFormat;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 //import java.text.DateFormat;
 import com.google.firebase.auth.FirebaseAuth;
@@ -41,6 +48,8 @@ public class ProgramsFragment extends Fragment{
 
     private HorizontalCalendar horizontalCalendar;
     private TextView dateSelected;
+    private TextView title;
+    private TextView programsScroll;
     private ScrollView dateScroll;
     private ListView dateListPrint;
     private ListView programList;
@@ -50,8 +59,8 @@ public class ProgramsFragment extends Fragment{
     private ArrayAdapter<String> programsAdapter;
 
     //array lists for dates
-    private ArrayList<Calendar> datesClicked;
-    private ArrayAdapter<Calendar> datesClickedAdapter;
+    //private ArrayList<Calendar> datesClicked;
+    //private ArrayAdapter<Calendar> datesClickedAdapter;
 
     //initialize and declare database reference
     private final FirebaseDatabase db = FirebaseDatabase.getInstance();
@@ -60,13 +69,16 @@ public class ProgramsFragment extends Fragment{
 
 
 
-    private ValueEventListener programListener = new ValueEventListener(){
+    /*private ValueEventListener programListener = new ValueEventListener(){
         @Override
         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            if (programsAdapter != null) {
+                programsAdapter.clear();
+            }
             try {
 
                 for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                    programsAdapter.addAll((String) ds.getKey());
+                    programsAdapter.addAll(ds.getKey());
                 }
 
 
@@ -78,27 +90,7 @@ public class ProgramsFragment extends Fragment{
         public void onCancelled(@NonNull DatabaseError databaseError){
 
         }
-    };
-
-    private ValueEventListener dateListener = new ValueEventListener(){
-        @Override
-        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-            try {
-
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                    datesClickedAdapter.add(Calendar.getInstance());
-                }
-
-
-            } catch (NullPointerException e) {
-
-            }
-        }
-        @Override
-        public void onCancelled(@NonNull DatabaseError databaseError){
-
-        }
-    };
+    };*/
 
     @NonNull
     @Override
@@ -107,6 +99,12 @@ public class ProgramsFragment extends Fragment{
         View rootView = inflater.inflate(R.layout.fragment_programs, container, false);
         dateSelected = rootView.findViewById(R.id.dateBox);
         //dateScroll = rootView.findViewById(R.id.dateScroll);
+        programList= rootView.findViewById(R.id.programList);
+        title = rootView.findViewById(R.id.title);
+        //programsScroll = rootView.findViewById(R.id.programScroll);
+
+        //initializing add program to selected date button
+        //Button addProgram = rootView.findViewById(R.id.addProgramToDate);
 
         ////////Calendar View Specifications////////
         /* starts before 1 month from now */
@@ -115,6 +113,8 @@ public class ProgramsFragment extends Fragment{
         /* ends after 1 month from now */
         Calendar endDate = Calendar.getInstance();
         endDate.add(Calendar.MONTH, 1);
+
+        //programsAdapter = new RecyclerView.Adapter<ArrayList<String>>(this.getContext(), android.R.layout.simple_list_item_1, programs);
 
         horizontalCalendar = new HorizontalCalendar.Builder(rootView, R.id.calendarView)
                 .range(startDate, endDate)
@@ -128,55 +128,55 @@ public class ProgramsFragment extends Fragment{
                     .showBottomText(true)
                     .textColor(Color.LTGRAY, Color.WHITE)
                 .end()
-                .addEvents(new CalendarEventsPredicate() {
-
-                    Random rnd = new Random();
-                    @Override
-                    public List<CalendarEvent> events(Calendar date) {
-                        List<CalendarEvent> events = new ArrayList<>();
-                        int count = rnd.nextInt(6);
-
-                        for (int i = 0; i <= count; i++){
-                            events.add(new CalendarEvent(Color.rgb(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)), "event"));
-                        }
-
-                        return events;
-                    }
-                })
                 .build();
 
         horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
             @Override
             public void onDateSelected(Calendar date, int position) {
                 dateSelected.setText(DateFormat.format("EEE, MMM d, yyyy", date));
-                //dateScroll.addView((View) DateFormat.format("EEE, MMM d, yyyy", date));
-                //initializing add program to selected date button
-                //Button addProgram = rootView.findViewById(R.id.addProgramToDate);
+                /* builder1.setTitle("Select Program");
+                //get current user id
+                String userid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                //declare programs reference
+                programs = new ArrayList<>();
+                userPrograms = db.getReference("Users").child(userid).child("Programs");
+                userPrograms.addListenerForSingleValueEvent(programListener);
 
-                //initializing dates selected list view
-                //dateListPrint = rootView.findViewById(R.id.dateOutputList);
-
-                //declare programs reference for user
-                //datesClicked = new ArrayList<Calendar>();
-                //datesClicked.add(date);
-                //userDates = db.getReference("Users").child(userid).child("Program Dates");
-                //userDates.addListenerForSingleValueEvent();
-                //DateListAdapter dla = new DateListAdapter(this, android.R.layout.simple_list_item_1, datesClicked);
-                //datesClickedAdapter = new ArrayAdapter<Calendar>(this,datesClicked );
-                //dateListPrint.setAdapter(dla);
-                //dateListPrint.setOnItemClickListener((AdapterView.OnItemClickListener) DateFormat.format("EEE, MMM d, yyyy", date));
+                programList.setAdapter(programsAdapter);
+                builder1.setNegativeButton("Cancel", (d, w) ->{
+                    d.cancel();
+                });
+                builder1.show();
+                */
             }
             @Override
             public boolean onDateLongClicked(Calendar date, int position) {
-                //get current user id
-                //String userid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser().getUid());
-                //declare programs reference
-                //userPrograms = db.getReference("Users").child(userid).child("Programs");
-                //userPrograms.addListenerForSingleValueEvent(programListener);
                 return true;
             }
         });
 
+        //add program click listener
+/*
+        addProgram.setOnClickListener(v1 -> {
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this.getContext(), R.style.AlertDialogStyle);
+            builder1.setTitle("Select Program");
+            //get current user id
+            String userid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            //declare programs reference
+            programs = new ArrayList<>();
+            userPrograms = db.getReference("Users").child(userid).child("Programs");
+            userPrograms.addListenerForSingleValueEvent(programListener);
+            programsAdapter = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_list_item_1, programs);
+
+            programList.setAdapter(programsAdapter);
+            builder1.setNegativeButton("Cancel", (d, w) ->{
+                d.cancel();
+            });
+            builder1.show();
+        });
+
+        */
         return rootView;
     }
+
 }
